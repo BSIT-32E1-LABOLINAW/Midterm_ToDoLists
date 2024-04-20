186a4017c221c06a0d2cc81c7ad086407f4b12b2
Cresencio_RodolfoMiguel_BSIT32E1 <migz.cresencio@gmail.com>
2024-04-21 02:03:07 +0800

    made public class service

---
+++ ToDo.Services/Services.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using ToDo.Domain;
+using ToDo.Repository;
+
+namespace ToDo.Services
+{
+    public class ToDoService
+    {
+        private readonly ToDoRepository _repository;
+
+        public ToDoService(ToDoRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
