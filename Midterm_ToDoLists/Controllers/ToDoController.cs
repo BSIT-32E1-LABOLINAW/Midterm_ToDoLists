d3d42b44adf6f7de86a730204822251755440755
De Luna, Mart Dexter <153422801+DeLuna-MartDexter-BSIT32E1@users.noreply.github.com>
2024-04-20 21:21:33 +0800

    Added a controller for TodoServices

---
+++ Midterm_ToDoLists/Controllers/ToDoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDo.Services;
+using ToDo.Repository;
+using ToDo.Domain;
+
+namespace Midterm_ToDoLists.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly ToDoService _toDoService;
+
+        public ToDoController(ToDoService toDoService)
+        {
+            _toDoService = toDoService;
+        }
+
+        public ActionResult Index()
+        {
+            var todos = _toDoService.GetAll();
+
+            // Add numbering to the todos
+            var numberedTodos = todos.Select((todo, index) =>
+                new Tuple<int, ToDo.Domain.ToDo>(index + 1, todo)).ToList();
+
+            return View(numberedTodos);
+        }
+    }
+}
