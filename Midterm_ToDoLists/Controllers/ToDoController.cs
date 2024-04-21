416d9ce3774c65618956071a8230c9ff2ae15c89
TabinJasper-BSIT32E1 <Tabinjasper@gmail.com>
2024-04-22 03:59:21 +0800

    Fixed some bugs, Now working

--- Midterm_ToDoLists/Controllers/ToDoController.cs
+++ Midterm_ToDoLists/Controllers/ToDoController.cs
@@ -3,68 +3,66 @@ using ToDo.Services;
 using ToDo.Repository;
 using ToDo.Domain;
 
-namespace Midterm_ToDoLists.Controllers
+public class ToDoController : Controller
 {
-    public class ToDoController : Controller
+    private readonly ToDoService _toDoService;
+
+    public ToDoController(ToDoService toDoService)
     {
-        private readonly ToDoService _toDoService;
+        _toDoService = toDoService;
+    }
 
-        public ToDoController(ToDoService toDoService)
+    public ActionResult Index()
+    {
+        var todos = _toDoService.GetAll();
+
+        // Add numbering to the todos
+        var numberedTodos = todos.Select((todo, index) =>
+            new Tuple<int, ToDo.Domain.ToDo>(index + 1, todo)).ToList();
+
+        return View(numberedTodos);
+    }
+
+    [HttpGet]
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(ToDo.Domain.ToDo todo)
+    {
+        if (string.IsNullOrWhiteSpace(todo.Task) || string.IsNullOrWhiteSpace(todo.Date))
         {
-            _toDoService = toDoService;
-        }
-
-        public ActionResult Index()
-        {
-            var todos = _toDoService.GetAll();
-
-            // Add numbering to the todos
-            var numberedTodos = todos.Select((todo, index) =>
-                new Tuple<int, ToDo.Domain.ToDo>(index + 1, todo)).ToList();
-
-            return View(numberedTodos);
-        }
-
-        [HttpGet]
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Create(ToDo.Domain.ToDo todo)
-        {
-            if (string.IsNullOrWhiteSpace(todo.Task) || string.IsNullOrWhiteSpace(todo.Date))
-            {
-                ModelState.AddModelError("", "Fields must not be empty");
-                return View(todo);
-            }
-            _toDoService.Create(todo);
-            return RedirectToAction("Index");
-        }
-
-        [HttpGet]
-        public ActionResult Edit(int id)
-        {
-            var todo = _toDoService.GetById(id);
+            ModelState.AddModelError("", "Fields must not be empty");
             return View(todo);
         }
+        _toDoService.Create(todo);
+        return RedirectToAction("Index");
+    }
 
-        [HttpPost]
-        public ActionResult Edit(ToDo.Domain.ToDo todo)
+    [HttpGet]
+    public ActionResult Edit(int id)
+    {
+        var todo = _toDoService.GetById(id);
+        return View(todo);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(ToDo.Domain.ToDo todo)
+    {
+        if (string.IsNullOrWhiteSpace(todo.Task) || string.IsNullOrWhiteSpace(todo.Date))
         {
-            if (string.IsNullOrWhiteSpace(todo.Task) || string.IsNullOrWhiteSpace(todo.Date))
-            {
-                ModelState.AddModelError("", "Fields must not be empty");
-                return View(todo);
-            }
-            _toDoService.Update(todo);
-            return RedirectToAction("Index");
+            ModelState.AddModelError("", "Fields must not be empty");
+            return View(todo);
         }
-        public ActionResult Delete(int id)
-        {
-            _toDoService.Delete(id);
-            return RedirectToAction("Index");
-        }
+        _toDoService.Update(todo);
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+        _toDoService.Delete(id);
+        return RedirectToAction("Index");
     }
 }
--- ToDo.Services/Services.cs
+++ ToDo.Services/Services.cs
@@ -7,32 +7,36 @@ namespace ToDo.Services
 {
     public class ToDoService
     {
-        private readonly ToDo.Repository _repository;
+        private readonly ToDoRepository _repository;
 
-        public ToDoService(ToDo.Repository repository)
+        public ToDoService(ToDoRepository repository)
         {
             _repository = repository;
         }
-    }
-    public Domain.ToDo Create(Domain.ToDo todo)
-    {
-        return _repository.Create(todo);
-    }
-    public Domain.ToDo GetById(int id)
-    {
-        return _repository.GetById(id);
-    }
-    public IEnumerable<Domain.ToDo> GetAll()
-    {
-        return _repository.GetAll();
-    }
-    public void Update(Domain.ToDo updatedTodo)
-    {
-        _repository.Update(updatedTodo);
-    }
-    public void Delete(int id)
-    {
-        _repository.Delete(id);
-    }
 
+        public Domain.ToDo Create(Domain.ToDo todo)
+        {
+            return _repository.Create(todo);
+        }
+
+        public Domain.ToDo GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public IEnumerable<Domain.ToDo> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public void Update(Domain.ToDo updatedTodo)
+        {
+            _repository.Update(updatedTodo);
+        }
+
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+    }
 }
